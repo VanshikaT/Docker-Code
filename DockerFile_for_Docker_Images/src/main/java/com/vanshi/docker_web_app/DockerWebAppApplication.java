2261ebcd299e6ba376b59e10588840cf9fc4a58b
VanshikaT <vanshikatri00@gmail.com>
2026-01-12 00:01:05 +0530

    Add dockerfile for Spring web app

---
+++ DockerFile_for_Docker_Images/src/main/java/com/vanshi/docker_web_app/DockerWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.vanshi.docker_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerWebAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DockerWebAppApplication.class, args);
+	}
+
+}
---
+++ DockerFile_for_Docker_Images/src/test/java/com/vanshi/docker_web_app/DockerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanshi.docker_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
